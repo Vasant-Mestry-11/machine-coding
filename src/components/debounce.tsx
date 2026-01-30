211ebb7eab5ff7fd11eb52a1a8455c8fed2053e1
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-01-30 20:31:52 +0530

    :zap: :art: added debounce functionality component

--- src/components/debounce.tsx
+++ src/components/debounce.tsx
@@ -1,7 +1,42 @@
+import React, { useEffect, useState } from "react";
+import styles from "../styles/debounce.module.css";
+
 const Debounce = () => {
+  const [input, setInput] = useState("");
+  const [debouncedText, setDebouncedText] = useState("");
+
+  useEffect(() => {
+    let timer;
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+      setDebouncedText(input);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
   return (
-    <div>
+    <div className={styles.debounceContainer}>
       <h1>Debounce</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={handleChange}
+        className={styles.debounceInput}
+      />
+      <div className={styles.display}>
+        <h3>{debouncedText}</h3>
+      </div>
     </div>
   );
 };
