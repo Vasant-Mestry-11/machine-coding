9e9937413a6110c547317ac554319fc663bf425b
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2026-01-30 20:04:45 +0530

    :zap: :art: first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Debounce from "./components/debounce";
+
+function App() {
+  return (
+    <>
+      <h1>Machine coding questions</h1>
+      <Debounce />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/debounce.tsx
@@ -0,0 +1,9 @@
+const Debounce = () => {
+  return (
+    <div>
+      <h1>Debounce</h1>
+    </div>
+  );
+};
+
+export default Debounce;
